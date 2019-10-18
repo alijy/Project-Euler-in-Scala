e8738ccb2643cabe531dfa28f8514b0e2799a1e2
AliKarami <karamiali19@gmail.com>
2019-10-18 08:54:22 +0100

    Euler001 added additional implementation and tests

--- src/main/scala/Euler001.scala
+++ src/main/scala/Euler001.scala
@@ -5,6 +5,31 @@
 
 object Euler001 extends App {
 
-  def sumOfMultiple(n: Int): Int = ((3 until n by 3) ++ (5 until n by 5)).toSet.sum
+  // solution 1
+  def sumOfMultiple(n: Int): Long = ((3 until n by 3) ++ (5 until n by 5)).toSet.sum
+
+  // solution 2
+  val d3 = LazyList.from(3).filter(_%3==0)
+  val d5 = LazyList.from(5).filter(_%5==0)
+  def sumOfMultiples(n: Int): Long = (d3.takeWhile(_<n) ++ d5.takeWhile(_<n)).toSet.sum
+
+  // solution 3
+  val d35: LazyList[Int] = LazyList.from(3).filter(x => x%3==0 || x%5==0)
+  def sumOfMultiples3(n: Int) = d35.takeWhile(_<n).sum
+
+  // solution 4
+  val s: LazyList[Long] = LazyList.from(3).collect[Long]{case n if n%3==0 || n%5==0 => n}
+  def sumOfMultiples4(n: Int) = s.takeWhile(_<n).sum
+
+  /*
+    solution 5
+    this solution calculates the result for n <= Max.Int (i.e. 2147483647)
+   */
+  def sumOfMultiples5(n: Long) = {
+    val sum3 = (1L to ((n - 1) / 3)).sum * 3
+    val sum5 = (1L to ((n - 1) / 5)).sum * 5
+    val sum15 = (1L to ((n - 1) / 15)).sum * 15
+    sum3 + sum5 - sum15
+  }
 
 }
--- src/test/scala/Euler001Spec.scala
+++ src/test/scala/Euler001Spec.scala
@@ -1,13 +1,51 @@
 import org.scalatest._
 
 class Euler001Spec extends FlatSpec {
+  "sumOfMultiple" should "sum of multiples of 3 and 5 up to 1, to 0" in {
+    assert(Euler001.sumOfMultiple(1) == 0)
+  }
   "sumOfMultiple" should "sum of multiples of 3 and 5 up to 10, to 23" in {
     assert(Euler001.sumOfMultiple(10) == 23)
   }
-  "sumOfMultiple" should "sum of multiples of 3 and 5 up to 100, to 23" in {
+  "sumOfMultiple" should "sum of multiples of 3 and 5 up to 100, to 2318" in {
     assert(Euler001.sumOfMultiple(100) == 2318)
   }
-  "sumOfMultiple" should "sum of multiples of 3 and 5 up to 1000, to 23" in {
+  "sumOfMultiple" should "sum of multiples of 3 and 5 up to 1000, to 233168" in {
     assert(Euler001.sumOfMultiple(1000) == 233168)
   }
+
+  "sumOfMultiple5" should "sum of multiples of 3 and 5 up to 1, to 0" in {
+    assert(Euler001.sumOfMultiples5(1) == 0L)
+  }
+  "sumOfMultiple5" should "sum of multiples of 3 and 5 up to 10, to 23" in {
+    assert(Euler001.sumOfMultiples5(10) == 23L)
+  }
+  "sumOfMultiple5" should "sum of multiples of 3 and 5 up to 100, to 2318" in {
+    assert(Euler001.sumOfMultiples5(100) == 2318L)
+  }
+  "sumOfMultiple5" should "sum of multiples of 3 and 5 up to 1000, to 233168" in {
+    assert(Euler001.sumOfMultiples5(1000) == 233168L)
+  }
+  "sumOfMultiple5" should "sum of multiples of 3 and 5 up to 10000, to 23331668" in {
+    assert(Euler001.sumOfMultiples5(10000) == 23331668L)
+  }
+  "sumOfMultiple5" should "sum of multiples of 3 and 5 up to 100000, to 2333316668" in {
+    assert(Euler001.sumOfMultiples5(100000) == 2333316668L)
+  }
+  "sumOfMultiple5" should "sum of multiples of 3 and 5 up to 1000000, to 233333166668" in {
+    assert(Euler001.sumOfMultiples5(1000000) == 233333166668L)
+  }
+  "sumOfMultiple5" should "sum of multiples of 3 and 5 up to 10000000, to 23333331666668" in {
+    assert(Euler001.sumOfMultiples5(10000000) == 23333331666668L)
+  }
+  "sumOfMultiple5" should "sum of multiples of 3 and 5 up to 100000000, to 2333333316666668" in {
+    assert(Euler001.sumOfMultiples5(100000000) == 2333333316666668L)
+  }
+  "sumOfMultiple5" should "sum of multiples of 3 and 5 up to 1000000000, to 233333333166666668" in {
+    assert(Euler001.sumOfMultiples5(1000000000) == 233333333166666668L)
+  }
+  "sumOfMultiple5" should "sum of multiples of 3 and 5 up to 2147483647, to 1076060070465310994" in {
+    assert(Euler001.sumOfMultiples5(2147483647) == 1076060070465310994L)
+  }
+
 }
